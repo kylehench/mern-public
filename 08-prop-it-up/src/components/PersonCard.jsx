09306946c287cad428af8454ea2ff58e84f447bb
Kyle Hench <kylehench@gmail.com>
2022-05-06 14:07:22 -0700

    added birthday button

--- 08-prop-it-up/src/components/PersonCard.jsx
+++ 08-prop-it-up/src/components/PersonCard.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PersonCard = (props) => {
+  const [age, setAge] = useState(props.age)
+  const addYear = () => setAge(age+1)
   return (
-    <div>
+    <div className='border border-primary rounded p-3 m-2'>
       <h2>{props.lastName}, {props.firstName}</h2>
-      <p>Age: {props.age}</p>
+      <p>Age: {age}</p>
       <p>Hair Color: {props.hairColor}</p>
+      <button onClick={() => addYear()} className="btn btn-primary">Birthday Button for {props.firstName} {props.lastName}</button>
     </div>
   )
 }
