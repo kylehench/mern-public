52d08e799cf9a7eba0a68f94ce2b4b6472a717b3
Kyle Hench <kylehench@gmail.com>
2022-05-06 10:40:46 -0700

    completed prop-it-up

--- 08-prop-it-up/src/App.js
+++ 08-prop-it-up/src/App.js
@@ -1,10 +1,13 @@
-import PersonCard from './components';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      
+      <PersonCard lastName="Doe" firstName="Jane" age={45} hairColor="Black" />
+      <PersonCard lastName="John" firstName="Smith" age={88} hairColor="Brown" />
+      <PersonCard lastName="Joe" firstName="Shmoe" age={22} hairColor="Brown" />
+      <PersonCard lastName="Bob" firstName="Ross" age={52} hairColor="Brown" />
     </div>
   );
 }
