d1aefc9fffce8548826722e8258f22f27a660099
Kyle Hench <kylehench@gmail.com>
2022-05-26 17:50:44 -0700

    completed faker api

--- 24-faker-api/server.js
+++ 24-faker-api/server.js
@@ -22,8 +22,9 @@ const newFakeProduct = createProduct();
 * }
 */
 
+// create user
 const createUser = () => {
-  const user = {
+  return {
     _id: faker.database.mongodbObjectId(),
     firstName: faker.name.firstName(),
     lastName: faker.name.firstName(),
@@ -31,17 +32,50 @@ const createUser = () => {
     phoneNumber: faker.phone.phoneNumber(),
     password: faker.internet.password(),
   }
-  return user
 }
 const newFakeUser = createUser();
-console.log(newFakeUser);
+// console.log(newFakeUser);
 
+// create company
+const createCompany = () => {
+  return {
+    _id: faker.database.mongodbObjectId(),
+    name: faker.name.firstName(),
+    address: {
+      street: faker.address.street(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zipCode: faker.address.zipCode(),
+      country: faker.address.country(),
+    },
+  }
+}
+
+// console.log(createCompany());
 
 // req is short for request
 // res is short for response
-app.get("/api", (req, res) => {
+app.get('/api', (req, res) => {
   res.send("Our express api server is now sending this over to the browser");
-});
+})
+
+// API for new user
+app.get('/api/users/new', (req, res) => {
+  res.send(createUser())
+})
+
+// API for new company
+app.get('/api/companies/new', (req, res) => {
+  res.send(createCompany())
+})
+
+// API for a new user and new company
+app.get('/api/user/company', (req, res) => {
+  res.send({
+    user: createUser(),
+    company: createCompany(),
+  })
+})
 
 const server = app.listen(8000, () =>
   console.log(`Server is locked and loaded on port ${server.address().port}!`)
