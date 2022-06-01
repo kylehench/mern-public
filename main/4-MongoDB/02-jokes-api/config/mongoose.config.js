1ca47063b837d67a0a16101865eae01556508f25
Kyle Hench <kylehench@gmail.com>
2022-05-31 17:59:50 -0700

    jokes-api initial commit

---
+++ main/4-MongoDB/02-jokes-api/config/mongoose.config.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/jokes-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Established a connection with the database'))
+  .catch( err => console.log('Something went wrong when connecting to the database', err))
+  
