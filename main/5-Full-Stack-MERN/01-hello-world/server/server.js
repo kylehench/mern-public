323425edab1a975b60ccb6ae1e667bea6544cdad
Kyle Hench <kylehench@gmail.com>
2022-06-06 16:58:36 -0700

    completed back-end

--- main/5-Full-Stack-MERN/01-hello-world/server/server.js
+++ main/5-Full-Stack-MERN/01-hello-world/server/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));   // This is new and allows JSON Objects with strings and arrays
+require('./config/mongoose.config')
 require('./routes/person.routes')(app)
 const port = 8000
 app.listen(port, () => console.log(`Listening on port: ${port}`) )
