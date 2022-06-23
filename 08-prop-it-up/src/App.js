1118c7b19720db20bbb86d7152cfc39750a99eb1
Kyle Hench <kylehench@gmail.com>
2022-06-23 11:00:28 -0700

    prop-it-up fixed reversed names

--- 08-prop-it-up/src/App.js
+++ 08-prop-it-up/src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <PersonCard lastName="Doe" firstName="Jane" age={45} hairColor="Black" />
-      <PersonCard lastName="John" firstName="Smith" age={88} hairColor="Brown" />
-      <PersonCard lastName="Joe" firstName="Shmoe" age={22} hairColor="Brown" />
-      <PersonCard lastName="Bob" firstName="Ross" age={52} hairColor="Brown" />
+      <PersonCard lastName="Smith" firstName="John" age={88} hairColor="Brown" />
+      <PersonCard lastName="Shmoe" firstName="Joe" age={22} hairColor="Brown" />
+      <PersonCard lastName="Ross" firstName="Bob" age={52} hairColor="Brown" />
     </div>
   );
 }
