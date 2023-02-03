f598a44ee6258cb0229da9a0ed42d0e24fa0586e
Kyle Hench <kylehench@gmail.com>
2023-02-02 23:13:45 -0800

    fixed css issues

--- test-kitchen/01-react-grid-layout/client/src/components/Component.jsx
+++ test-kitchen/01-react-grid-layout/client/src/components/Component.jsx
@@ -1,33 +1,32 @@
 import React from 'react'
-import GridLayout, {Responsive, WidthProvider} from "react-grid-layout"
+import {Responsive, WidthProvider} from "react-grid-layout"
 import styled from "styled-components"
+import "/node_modules/react-grid-layout/css/styles.css";
+import "../index.css"
 
 const ResponsiveGridLayout = WidthProvider(Responsive)
 
 const layout = [
-  { i: "blue-eyes-dragon", x: 0, y: 0, w: 1, h: 1, isResizable: true },
+  { i: "blue-eyes-dragon", x: 0, y: 0, w: 1, h: 1 },
   { i: "dark-magician", x: 1, y: 0, w: 1, h: 1 },
   { i: "kuriboh", x: 2, y: 0, w: 1, h: 1 },
   { i: "spell-caster", x: 3, y: 0, w: 1, h: 1 },
-  { i: "summoned-skull", x: 4, y: 0, w: 1, h: 1, resizeHandles: ['se', 'n'] }
+  { i: "summoned-skull", x: 4, y: 0, w: 1, h: 1 }
 ]
 
 const GridItemWrapper = styled.div`
   background: #f5f5f5;
-  transition-property: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  transition-duration: 150ms;
+  // transition-property: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter;
+  // transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  // transition-duration: 150ms;
 `
 
 const GridItemContent = styled.div`
-  padding: 8px;
-  transition-property: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  transition-duration: 150ms;
+  // padding: 80px;
 `
 
 const Root = styled.div`
-  padding: 16px;
+  // padding: 160px;
 `
 
 const Component = () => {
@@ -37,29 +36,27 @@ const Component = () => {
         layouts={{ lg: layout }}
         breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
         cols={{ lg: 5, md: 4, sm: 3, xs: 2, xxs: 1 }}
-        rowHeight={300}
-        width={1000}
-        isResizable={true}
-        resizeHandles={['se', 's', 'n']}
-        className='react-resizable-handle'
+        rowHeight={50}
+        // width={100}
+        draggableHandle=".grid-item__title"
       >
-      <GridItemWrapper key="blue-eyes-dragon">
-          <GridItemContent>
-            <span>Blue Eyes Dragon</span>
-            <div onClick={e => 1}>Some more text, maybe a form in the future</div>
-          </GridItemContent>
-        </GridItemWrapper>
+        <div key="blue-eyes-dragon">
+            <div className="grid-item__title">
+              DRAG ME
+            </div>
+
+        </div>
         <GridItemWrapper key="dark-magician">
-          <GridItemContent>Dark Magician</GridItemContent>
+          <GridItemContent>Not You Gee Oh</GridItemContent>
         </GridItemWrapper>
         <GridItemWrapper key="kuriboh">
-          <GridItemContent>Kuriboh</GridItemContent>
+          <GridItemContent>Not You Gee Oh</GridItemContent>
         </GridItemWrapper>
         <GridItemWrapper key="spell-caster">
-          <GridItemContent>Spell Caster</GridItemContent>
+          <GridItemContent>Not You Gee Oh</GridItemContent>
         </GridItemWrapper>
         <GridItemWrapper key="summoned-skull">
-          <GridItemContent>Summoned Skull</GridItemContent>
+          <GridItemContent>Not You Gee Oh</GridItemContent>
         </GridItemWrapper>
       </ResponsiveGridLayout>
     </div>
--- test-kitchen/01-react-grid-layout/readme.md
+++ test-kitchen/01-react-grid-layout/readme.md
@@ -1,2 +1,6 @@
 # A testing area of the greact-grid-layout library
 Follows tutorial at: https://isamatov.com/react-grid-layout-tutorial
+
+# Great implemenatation example
+https://github.com/IntranetFactory/DnDBoard2
+https://codesandbox.io/s/draggable-resizable-dashboard-zf3hx?file=/src/containers/Dashboard.js:794-809
